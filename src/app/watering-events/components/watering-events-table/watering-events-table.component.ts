5ad2517a8d3542fc85c9bbc4213f9098fcd991d9
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2024-11-09 13:52:45 +0100

    Added paging for watering events

--- src/app/watering-events/components/watering-events-table/watering-events-table.component.ts
+++ src/app/watering-events/components/watering-events-table/watering-events-table.component.ts
@@ -11,12 +11,19 @@ import { ConfirmDialogComponent } from '../../../shared/components/confirm-dialo
 import { DialogData } from '../../../shared/interfaces/dialog-data';
 import { PagedResponse } from '../../../shared/interfaces/paged-response';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-watering-events-table',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatButtonModule, MatIconModule, MatCardModule],
+  imports: [
+    MatTableModule, 
+    CommonModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatCardModule, 
+    MatPaginatorModule
+  ],
   templateUrl: './watering-events-table.component.html',
   styleUrl: './watering-events-table.component.scss'
 })
@@ -28,30 +35,49 @@ export class WateringEventsTableComponent implements OnInit, OnChanges {
   @Input() plantId: number | undefined;
   @Input() reloadTrigger: number = 0; //when watering event in "details" page is added reload trigger is changed.
 
+  // Pagination
+  wateringEventsListLength = 0;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [10, 20, 50];
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
   constructor(
     private wateringEventService: WateringEventService,
     private notificationService: NotificationService,
     public dialog: MatDialog
   ) { }  
 
+  handlePageEvent(e: PageEvent) {
+    this.wateringEventsListLength = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    this.loadWateringEvents(this.pageIndex, this.pageSize);
+  }
+
   ngOnInit(): void {
-    this.loadWateringEvents();
+    this.loadWateringEvents(this.pageIndex, this.pageSize);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['reloadTrigger'] && !changes['reloadTrigger'].firstChange) {
-      this.loadWateringEvents();
+      this.loadWateringEvents(this.pageIndex, this.pageSize);
     }
   }
 
-  loadWateringEvents(): void {
+  loadWateringEvents(pageIndex: number, pageSize: number): void {
     if (!this.plantId) {
       return;
     }
 
-    this.wateringEventService.getAllWateringEventsByPlantId(this.plantId, 1, 10).subscribe((pagedResponse: PagedResponse<WateringEvent>) => {
+    this.wateringEventService.getAllWateringEventsByPlantId(this.plantId, pageIndex + 1, pageSize).subscribe((pagedResponse: PagedResponse<WateringEvent>) => {
       this.wateringEvents = pagedResponse.data;
       this.dataSource.data = this.wateringEvents;
+
+      this.wateringEventsListLength = pagedResponse.totalRecords;
     });
   }
 
@@ -74,8 +100,7 @@ export class WateringEventsTableComponent implements OnInit, OnChanges {
   doDeleteWateringEvent(eventId: number): void {
     this.wateringEventService.deleteWateringEvent(eventId).subscribe({
       next: () => {
-        this.wateringEvents = this.wateringEvents.filter(event => event.id !== eventId);
-        this.dataSource.data = this.dataSource.data.filter(event => event.id !== eventId);
+        this.loadWateringEvents(this.pageIndex, this.pageSize);
         this.notificationService.showNonErrorSnackBar('Watering event deleted');
       },
       error: (err) => {
